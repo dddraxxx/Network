c5911f87ed08cf9dca77521490c7c267bf089152
DONG <15207615513@qq.com>
2021-07-05 01:41:23 +0800

    proto net modules first update

--- proto/net.py
+++ proto/net.py
@@ -4,6 +4,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+''' Contrast feature map '''
+class CFM(nn.module):
+    def __init__(self) -> None:
+        super().__init__()
+
 ''' Channel based relation '''
 class CA(nn.Module):
     def __init__(self, in_channel_left, in_channel_up):
@@ -14,16 +19,20 @@ class CA(nn.Module):
         self.conv2  = nn.Conv2d(256, 256, 1, 1, 1)
 
     def forward(self, left, up):
-        left= F.relu(self.bn0(self.conv0(left)))  
+        left= F.relu(self.bn0(self.conv0(left)), inplace=True)  
         up  = up.mean(dim=(2,3), keepdim=True)
-        up  = F.relu(self.conv1(up))
-        up  = torch.tanh(self.conv2(up))
-        return left * up + left
+        up  = F.relu(self.conv1(up), inplace=True)
+        up  = torch.sigmoid(self.conv2(up))
+        return left * up
 
 """ Relational Attention module """
 class RAM(nn.module):
-    def __init__(self, ):
-        super().__init__()    
+    def __init__(self, in_channels):
+        super(RAM, self).__init__()    
+        self.conv0  = nn.Conv2d(in_channels, in_channels // 8, 1)
+        self.conv1  = nn.Conv2d(in_channels, in_channels // 8, 1)
+        self.conv2  = nn.Conv2d(in_channels, in_channels, 1)
+        self.softmax= nn.Softmax(dim=-1)
     
     def forward(self, left, up):
         assert left.size() == up.size()
@@ -36,15 +45,61 @@ class RAM(nn.module):
 
         return out
 
+""" Attention Map Decoder """
+class ChannelPool(nn.module):
+    def forward(self, x):
+        return x.mean(dim=1).unsqueeze(1)
+
+class AMD(nn.module):
+    def __init__(self, in_channel) -> None:
+        super(AMD, self).__init__()
+        self.conv0  = nn.Conv2d(in_channel, in_channel, 1, 1, 0)
+        self.bn0    = nn.BatchNorm2d(in_channel)
+        self.pool   = ChannelPool()
+
+    def forward(self, x):
+        x   = F.relu(self.bn0(self.conv0(x)), inplace=True)
+        x   = self.pool(x)
+        return x
 
 
+""" Refine attention map """
+class RFM(nn.module):
+    def __init__(self, in_channel_left, in_channel_up) -> None:
+        super(RFM, self).__init__()
+        self.conv0  = nn.Conv2d(in_channel_left, 512, 3, 1, 1)
+        self.bn0    = nn.BatchNorm2d(512)
+        self.conv2  = nn.Conv2d(in_channel_up, 256, 3, 1, 1)
+    
+    def forward(self, left, up):
+        out1    = F.relu(self.bn0(self.conv0(left)), inplace=True)
+        out2    = self.conv2(up)
+        w, b    = out1[:, :256, :, :], out1[:, 256:, :, :]
+        return F.relu(w * out2 + b, inplace=True)
+
+''' Supress-Augment module '''
+class SAM(nn.module):
+    def __init__(self, in_channel_left, in_channel_up) -> None:
+        super(SAM, self).__init__()
+        self.conv0  = nn.Conv2d(in_channel_left, in_channel_up, 1, 1, 0)
+        self.bn0    = nn.BatchNorm2d(in_channel_up)
+        self.conv1  = nn.Conv2d(in_channel_up, in_channel_up, 3, 1, 1)
+        self.bn1    = nn.BatchNorm2d(in_channel_up)
+        self.conv2  = nn.Conv2d(in_channel_up, in_channel_up, 3, 1, 1)
+        self.bn2    = nn.BatchNorm2d(in_channel_up)
+        self.conv3  = nn.Conv2d(in_channel_up, in_channel_up, 3, 1, 1)
+        self.bn3    = nn.BatchNorm2d(in_channel_up)
+        self.ram    = RAM(in_channel_up)
+
+    def forward(self, left, up):
+        left= F.relu(self.bn0(self.conv0(left)), inplace=True)
+        up  = F.relu(self.bn1(self.conv1(up)), inplace=True)
+        prod= up * left
+        prod1= F.relu(self.bn2(self.conv2(prod)), inplace=True)
+        up  = up - prod1
+        prod2= F.relu(self.bn3(self.conv3(prod)), inplace=True)
+        out  = self.ram(prod2, up)
+
+        return out
 
 
-# 
-
-# Relational attention score
-class 
-
-# Refine map
-
-# Contrast
